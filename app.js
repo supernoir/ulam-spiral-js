6d895bbd9eef16a6d8068ff645d0a2444c734b9b
supernoir <kawohl.bentodesign@gmail.com>
2016-05-08 00:11:42 +0200

    tries to build check for prime

--- app.js
+++ app.js
@@ -2,27 +2,38 @@
 
 var app = {}
 
-var getCanvas = function(){
-        var canvas = $('#canvas-scope');
+var drawCanvas = function() {
+        var canvas = document.getElementById('canvas-scope');
         var width = $('#canvas-scope').width();
         var height = $('#canvas-scope').height();
         
+                
         if (canvas.getContext){
           var ctx = canvas.getContext('2d');
-          
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
+        
+          ctx.fillStyle = "rgb(200,0,0)";
+          ctx.fillRect (10, 10, 55, 50);
+        
+        var value = 0;
+        for(var i = 0; i < 100; i++) {
+          if(value%i === 0) {
+            ctx.fillStyle = "rgb(255,255,255)";
+            ctx.fillRect(i*10,10,5,5);
+            console.log(i);
+          } else {
+            ctx.fillStyle = "rgb(200,0,0)";
+            ctx.fillRect(i*10,10,5,5);
+          }
         }
         
-        app.canvasloaded = canvas.selector;
-        console.log(app.canvasloaded);
+      
+      
+        }
         
-        return app.canvasloaded
+        
+        return canvas
       }
      
 
 
-module.exports = { getCanvas: getCanvas };
+module.exports = { drawCanvas: drawCanvas };
